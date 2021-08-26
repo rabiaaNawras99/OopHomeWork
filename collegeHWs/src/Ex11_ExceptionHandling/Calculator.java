42987bb78d86b2b00a4732e1f9db5b3aaa54fa6b
nawras <nawrasrabeeaa@gmail.com>
2021-08-26 10:05:42 +0300

    /

--- collegeHWs/src/Ex11_ExceptionHandling/Calculator.java
+++ collegeHWs/src/Ex11_ExceptionHandling/Calculator.java
@@ -10,16 +10,49 @@ public class Calculator {
 			throw new InvalidParameterException("the parameters must be 0-100");
 		}
 		if(num1+num2>100|| num2+num1<0) {
-			throw new InvalidParameterException("");
+			throw new InvalidParameterException("the result must be 0-100");
 		}
 		return num1+num2;
 	}
 	
-
-	public static int divide(int num1,int num2) {
-		return 9;
+	public static int subtract(int num1,int num2) throws InvalidParameterException{
 		
+		if(num1<0|| num1>100|| num2<0||num2>100) {
+			throw new InvalidParameterException("the parameters must be 0-100");
+		}
+		if(num1-num2>100|| num2-num1<0) {
+			throw new InvalidParameterException("the result must be 0-100");
+		}
+		return num1-num2;
 	}
+	
+	
+
+	public static double divide(int num1,int num2) throws ArithmeticException ,InvalidParameterException{
+		if (num2==0) {
+			throw new ArithmeticException("you cant divide by zero!!");
+		}
+		if(num1/num2<0|| num2/num1>0) {
+			throw new InvalidParameterException("the result must be 0-100");
+		}
+		return num1/num2;
+	}
+	
+		
+	public static int multiplication(int num1,int num2) throws InvalidParameterException{
+		
+		if(num1<0|| num1>100|| num2<0||num2>100) {
+			throw new InvalidParameterException("the parameters must be 0-100");
+		}
+		if(num1*num2>100|| num2*num1<0) {
+			throw new InvalidParameterException("the result must be 0-100");
+		}
+		return num1*num2;
+	}
+	
+	
+	
+	
 
 
 
